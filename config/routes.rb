c6a8c6177cebc695ddbe2b80e62f4676aad6ff11
ahng1996 <andy.h.nguyen1996@gmail.com>
2016-01-12 02:00:46 +0000

    search friend from ui

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
   get 'my_friends', to: "users#my_friends"
+  get 'search_friends', to: "user#search"
+  get 'add_friends', to: "user#add_friend"
 end
